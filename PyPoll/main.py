506e5155754d8220db5e8ced4bdb754bf2e8ddad
FrankJiang1208 <frankjiang1208@gmail.com>
2021-06-20 17:47:42 -0700

    Final Code without comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,46 @@
 import os
 import csv
+totalVotes=0
+candidates=[]
+candidatesVote=[]
+candidatePer=[]
 
 
 cwd = os.getcwd()
 csvPath=os.path.join(cwd,"PyPoll","Resources","election_data.csv")
 with open(csvPath,newline="") as csvFile:
 	csvReader=csv.reader(csvFile,delimiter=',')
-
+	next(csvReader)
 	for row in csvReader:
-		print(row)
+		name=row[2]
+		if (name not in candidates):
+			candidates.append(name)
+			candidatesVote.append(0)
+		else:
+			position=candidates.index(name)
+			candidatesVote[position]+=1
+
+	totalVotes=sum(candidatesVote)
+	for candidate in candidatesVote:
+		candidatePer.append("{:.0%}".format(candidate/totalVotes))
+	
+	winner=candidates[candidatesVote.index(max(candidatesVote))]
+
+
+print("Election Results\n-------------------------\nTotal Votes: "+str(totalVotes)+"\n-------------------------\n")
+for i in candidates:
+	print(i+":"+str(candidatePer[candidates.index(i)])+" ( "+str(candidatesVote[candidates.index(i)])+")\n")
+print("-------------------------\n")
+print("Winner: "+winner)
+
+
+
+txtPath=os.path.join(cwd,"PyPoll","Analysis","analysis.txt")
+txtFile=open(txtPath,"w")
+txtFile.write("Election Results\n-------------------------\nTotal Votes: "+str(totalVotes)+"\n-------------------------\n")
+for i in candidates:
+	txtFile.write(i+":"+str(candidatePer[candidates.index(i)])+" ( "+str(candidatesVote[candidates.index(i)])+")\n")
+txtFile.write("-------------------------\n")
+txtFile.write("Winner: "+winner)
+txtFile.close()
 
